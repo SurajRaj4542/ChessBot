1888cc1a6af86320d18fa12faa8f1a55baa6fc20
ほうたろう オレキ <surajraj12444@gmail.com>
2021-05-11 13:51:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,27 @@
 class Strings:
-    START_TEXT = """Hello, I'm Anime World Chess Bot  (~˘▾˘)~"""
+    START_TEXT = """Hello, I'm Anime World Chess Bot  (~˘▾˘)~
+    
+    Created by @Join_AnimeWorld for Managing Wins & Loss in Our <a href="https://t.me/JoinChessWorld">Chess group</a>
+    Learn How to use me by using the buttons below """
 
     GROUP_START_TEXT = """Hello, Nadeko is alive, 
 Checkout /help for getting instructions and useful features (≧▽≦)"""
 
-    HELP_TEXT = """SoonTM, Not Today OFC"""
+    HELP_TEXT = """Here's a bunch of Command Helper 💙
+/start - start the bot check if alive or not
+/help -  get bunch of help 
+/add - add strength to your fellow adventurer 
+/strength - check your strength 
+/info - get your or user's info
+  Hope, You'll Join your Stay in our <a herf="https://t.me/JoinCHessWorld">Chess/Gaming Group</a>"""
 
-    ABOUT_TEXT = """SoonTM, Not Today OFC"""
+    ABOUT_TEXT = """An Interesting Statistics Management Bot Made with ❤️ By @JoinAnimeWorld.
+Special Thanks To:-
+Coded by @Am_Suraj and @Anime_Is_Shit
+Support or Query Heads to @AW_SupportChat
+Thank You for your support ≧◡≦"""
 
-    UPDATES_TEXT = """SoonTM, Not Today OFC"""
+    UPDATES_TEXT = """Join Our Updates Channel Right Away to get more information about Bot."""
 
     INFO_TEXT = """Information about the User:
 ━━━━━━━━━━━━━━━━━━━
